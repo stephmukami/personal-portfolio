25d72aed193b588e457ac4c7d0d5a500604153d6
stephmukami <stephmukami@gmail.com>
2024-12-17 11:37:47 +0300

    feature: add nav responsiveness

--- personal-portfolio/src/app/(components)/Navbar.tsx
+++ personal-portfolio/src/app/(components)/Navbar.tsx
@@ -4,16 +4,16 @@ type Props = {}
 
 function Navbar({}: Props) {
   return (
-    <div className='flex-parent flex'>
-        <div className="written-name">
+    <div className='flex-parent font-medium flex flex-col md:flex-row  space-y-3 md:space-x-[1050px] align-center border border-emerald-400 p-4'>
+        <div className="written-name border border-red-400">
         <h4 className="handwritten {styles.Phitgate} text-5xl">Stephanie Mukami</h4>
         </div>
 
-        <div className="nav-links flex">
+        <div className="nav-links flex flex-col space-y-2 md:flex-row md:space-x-[50px] justify-center align-center border border-red-700 ">
             <h4>Past Work</h4>
             <h4>Side Projects</h4>
+            <h4>Communities</h4>
             <h4>Contact</h4>
-            <h4>Media</h4>
 
         </div>
 
