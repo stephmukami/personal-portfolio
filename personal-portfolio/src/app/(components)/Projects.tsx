51f5b6ac8a19fc1dc3aa64275de9f4a2afcd25fd
stephmukami <stephmukami@gmail.com>
2024-12-19 00:27:49 +0300

    feature:add resume and project section

--- personal-portfolio/src/app/(components)/Projects.tsx
+++ personal-portfolio/src/app/(components)/Projects.tsx
@@ -9,23 +9,63 @@ function Projects({}: Props) {
     <h2 className='text-xl font-semibold mb-4 text-center mt-3'>Side Projects</h2>
 
     <div className="flex-parent flex flex-col md:flex-row border border-red-600">
-        <div className="flex-child-image flex flex-col border border-fuchsia-500 p-4">
-            <div>Project Title</div>
-            <img src="./prisma.png" className='w-[40px] h-[40px]' alt="" />
-
+        <div className="flex-child-image flex flex-col border border-fuchsia-500 max-w-[400px] w-full p-4">
+            <div className='font-semibold mb-1'>E-Points</div>
+            <img src="./epoints-app-p.PNG" className="w-full h-[200px] object-contain" alt="E-Points App" />
         </div>
-        <div className="flex-child-text flex flex-col">
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, quam. Explicabo quos maxime nisi assumenda debitis perspiciatis voluptates
-                 facere consequuntur dolorem, ea aspernatur repellendus minus, non aliquam omnis, adipisci ut.
-            </div>
-            <div className="techs flex">
-                <div className="bg-tag-purple">tag</div>
-            </div>
-
+        <div className="flex-child-text flex flex-col p-3 justify-center">
+        <ul>
+            <li className="mb-1">- The app uses a gamified approach to promote the usage of renewable energy by rewarding users for eco-friendly transportation.</li>
+            <li className="mb-1">- Users earn points for each electric motorbike ride that can be redeemed for airtime, offering a tangible reward for environmentally conscious behavior.</li>
+            <li className="mb-1">- Built using Next.js for the framework, Prisma ORM for database management, and Africa's Talking Airtime API for airtime distribution.</li>
+        </ul>
         </div>
     </div>
 
+    <div className="flex-parent flex flex-col md:flex-row border border-red-600">
+        <div className="flex-child-image flex flex-col border border-fuchsia-500 max-w-[400px] w-full p-4">
+            <div>Medbot</div>
+            <img src="./medbot-app-p.PNG" className="w-full h-[200px] object-contain" alt="Medbot App" />
+        </div>
+        <div className="flex-child-text flex flex-col p-3 justify-center">
+        <ul>
+            <li className="mb-1">- Computer-aided diagnosis tool designed to diagnose pneumonia and tuberculosis from chest X-ray images.</li>
+            <li className="mb-1">- Built using Next.js for the web framework and FastAPI for the backend API services.</li>
+            <li className="mb-1">- Integrated DenseNet, a deep learning computer vision model, for image classification and diagnosis.</li>
+        </ul>
+        </div>
+    </div>
+
+    <div className="flex-parent flex flex-col md:flex-row border border-red-600">
+        <div className="flex-child-image flex flex-col border border-fuchsia-500 max-w-[400px] w-full p-4">
+            <div>iPrevent</div>
+            <img src="./iprevent-app-p.PNG" className="w-full h-[200px] object-contain" alt="iPrevent App" />
+        </div>
+        <div className="flex-child-text flex flex-col justify-center">
+        <ul>
+            <li className="mb-1">- ML-powered app for assessing diabetes risk level among adults.</li>
+            <li className="mb-1">- Built using Next.js for the front-end, Neon.db for database management, and Flask for the back-end API services.</li>
+            <li className="mb-1">- Utilizes an ADA boosted algorithm for diabetes risk prediction.</li>
+        </ul>
+
+            
+        </div>
+    </div>
+
+    <div className="flex-parent flex flex-col md:flex-row border border-red-600">
+        <div className="flex-child-image flex flex-col border border-fuchsia-500 max-w-[400px] w-full p-4">
+            <div>Kikwetu Store</div>
+            <img src="./kikwetu-app-p.PNG" className="w-full h-[200px] object-contain" alt="Kikwetu Store App" />
+        </div>
+        <div className="flex-child-text flex flex-col justify-center">
+        <ul>
+            <li className="mb-1">- E-commerce store built with React for the front-end and MongoDB for database management.</li>
+            <li className="mb-1">- Focuses on handling the checkout process for user items.</li>
+        </ul>
+
+          
+        </div>
+    </div>
 
     </div>
     </>
--- personal-portfolio/src/app/(components)/Resume.tsx
+++ personal-portfolio/src/app/(components)/Resume.tsx
@@ -6,35 +6,30 @@ function Resume({}: Props) {
   return (
     <>
       <h2 className='text-xl font-semibold mb-8 text-center'>Past Work</h2>
-      <div className="flex flex-col justify-center items-center flex-parent">
+      <div className="flex flex-col justify-center items-center flex-parent mb-6">
 
-        <div className="resume-tab bg-brand-grey md:w-[1600px] rounded-sm mb-2 ">
+        <div className="resume-tab bg-brand-grey md:w-[1600px] rounded-md mb-2 ">
           <h4 className="exp-header font-semibold p-3">Robert Bosch - Innovation Analyst</h4>
           <ul className='mb-4 p-3'>
             <li className='mb-1'>- Proposed a USSD-based verification system to reduce revenue loss from counterfeit power tools; tested with 30 artisans and 5 Bosch Power Tools stakeholders.</li>
-            <li className='mb-1'>- Led technical discovery with 3 law firms across North and South Africa through 6 prototyping sessions, identifying potential to reduce legal research costs by 50%.</li>
             <li className='mb-1'>- Developed system requirements and user stories for an AI-powered legal research system, collaborating with 20 legal professionals and business stakeholders.</li>
             <li className='mb-1'>- Designed and delivered generative AI training to a cross-functional team of 33 colleagues, focusing on practical implementation strategies for various Bosch business units.</li>
           </ul>
         </div>
 
-        <div className="resume-tab bg-brand-grey md:w-[1600px] rounded-sm mb-2">
+        <div className="resume-tab bg-brand-grey md:w-[1600px] rounded-md  rounmb-2">
           <h4 className="exp-header font-semibold mb-3 p-3">Data Trace International - Software Engineer</h4>
           <ul className='mb-4 p-3'>
             <li className='mb-1'>- Led design and development of the KenVIP responsive web portal aimed at digitizing national vaccination coverage records and enhancing decision-making within the Ministry of Health.</li>
-            <li className='mb-1'>- Consolidated vaccination coverage data from 6 Ministry of Health sources, employing ETL tools like NiFi for cleaning and PostgreSQL for data storage.</li>
-            <li className='mb-1'>- Developed 13 national vaccine coverage dashboards using SQL and Superset, enabling data-driven insights.</li>
             <li className='mb-1'>- Built a comprehensive data pipeline to streamline data processing and storage, improving data accuracy and accessibility.</li>
           </ul>
         </div>
 
-        <div className="resume-tab bg-brand-grey md:w-[1600px] rounded-sm mb-2">
+        <div className="resume-tab bg-brand-grey md:w-[1600px] rounded-md mb-2">
           <h4 className="exp-header font-semibold mb-3 p-3">Ingressive For Good - Apprentice</h4>
           <ul className='mb-4 p-3'>
             <li className='mb-1'>- Awarded a scholarship by Ingressive for Good, selected as 1 of 1,000 recipients from 19,500 African applicants.</li>
-            <li className='mb-1'>- Completed a month-long virtual training in UI/UX design, delivering milestone projects including:</li>
-              <li className='mb-1'>- An e-commerce website prototype emphasizing user-centric design and functionality.</li>
-              <li className='mb-1'>- A health and wellness app prototype prioritizing user experience and accessibility.</li>
+            <li className='mb-1'>- Completed a month-long virtual training in UI/UX design, delivering milestone projects including and e-commerce and health and wellness app prototype</li>
           </ul>
         </div>
       </div>
